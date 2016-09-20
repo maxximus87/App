06a1b64d54430f0c3a0914508c519ce55c195e5e
maxximus87 <maxxturner10@gmail.com>
2016-09-20 15:56:32 -0400

    app2

--- app.rb
+++ app.rb
@@ -16,5 +16,22 @@ end
 
 post '/age' do
 	age = params[:user_age]
-	  "You are #{age} years old."
+    name = params[:user_name]
+	    redirect '/3_fav_numbers?name=' + name + '&age=' + age
 end
+
+ get '/3_fav_numbers' do
+ 	name = params[:user_name]
+ 	age = params[:user_age]
+	erb :get_favorite, :locals => {:name => name, :age => age,}
+ end
+
+ post '/numbers' do
+ 	age = params[:age]
+	name = params[:name]
+	fav1 = params[:user_number_1]
+	fav2 = params[:user_number_2]
+	fav3 = params[:user_number_3]
+	"Your favorite numbers are #{fav1}, #{fav2}, and #{fav3}!"
+end
+
